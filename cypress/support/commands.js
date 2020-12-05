1a4c25fddf95d19538bec211e79a6bfe2dbc2580
MelissaQA <meli21791@gmail.com>
2020-12-04 21:16:34 -0500

    Login custom command created

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (username, password) => {  
+    cy.get('#txtUsername').type(username);
+    cy.get('#txtPassword').type(password);
+    cy.get('#btnLogin').click();
+})
